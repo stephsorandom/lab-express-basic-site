d773c7d2b6246a374aa690a56f7765abdb21d46b
Stephanie Mitchell <stephsorandom@gmail.com>
2021-02-23 17:02:37 -0500

    new links

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const app = express();
 
 app.get("/home", (request, response) => {
   response.sendFile(__dirname + "/views/home-page.html");
-  response.send(__dirname + "/views/about-page.html")
+});
+
+app.get("/about", (request, response) => {
+    response.sendFile(__dirname + "/views/about-page.html");
+});
+
+  app.get("/works", (request, response) => {
+    response.sendFile(__dirname + "/views/works-page.html");
 });
 
 const PORT = process.env.PORT || 3000;
